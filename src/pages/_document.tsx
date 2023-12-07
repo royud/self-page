078e21564e061bcb8943cb8fee8f9a1ca7ed7e07
이재정 <jj9535@naver.com>
2023-12-07 19:41:30 +0900

    배포 에러 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
           <link
             rel="stylesheet"
             as="style"
-            crossorigin
+            crossOrigin
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
           />
         </Head>
